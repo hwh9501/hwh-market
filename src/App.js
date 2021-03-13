be87058723165b6dbc81907e78346780f97d05e0
hwh <zaq8077@naver.com>
2021-03-13 15:10:54 +0900

    link 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 import MainPages from "./main";
+import { Switch, Route } from "react-router-dom";
+import ProductPage from "./product";
+import uploadPage from "./upload";
+
 function App() {
-  return <MainPages />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPages />;
+        </Route>
+        <Route exact={true} path="/product/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <uploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
