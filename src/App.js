1093d7c9be6cf129a48ec53e036bb5a24037f51f
hwh <zaq8077@naver.com>
2021-03-27 21:29:59 +0900

    antd 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import "antd/dist/antd.css";
 import "./App.css";
 import MainPages from "./main";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import ProductPage from "./product";
-import uploadPage from "./upload";
+import UploadPage from "./upload";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" />
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품 업로드
+          </Button>
         </div>
       </div>
       <div id="body">
@@ -20,8 +35,9 @@ function App() {
           <Route exact={true} path="/product/:id">
             <ProductPage />
           </Route>
+
           <Route exact={true} path="/upload">
-            <uploadPage />
+            <UploadPage />
           </Route>
         </Switch>
       </div>
